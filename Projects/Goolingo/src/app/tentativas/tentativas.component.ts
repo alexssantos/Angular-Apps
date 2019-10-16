a25924609bfd9809e848e1aea054a0333c79adb6
Alex Santos <arkanjo.alex@gmail.com>
2019-10-16 18:28:05 -0300

    fix format

--- Projects/Goolingo/src/app/tentativas/tentativas.component.ts
+++ Projects/Goolingo/src/app/tentativas/tentativas.component.ts
@@ -2,42 +2,49 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Coracao } from '../shared/coracao.model';
 
 @Component({
-  selector: 'app-tentativas',
-  templateUrl: './tentativas.component.html',
-  styleUrls: ['./tentativas.component.css']
+	selector: 'app-tentativas',
+	templateUrl: './tentativas.component.html',
+	styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit, OnChanges {
+export class TentativasComponent implements OnInit, OnChanges
+{
 
-  @Input() public tentativas: number;
+	@Input() public tentativas: number;
 
-  public arrayCoracoes: Coracao[] = [
-    new Coracao(true),
-    new Coracao(true),
-    new Coracao(true)
-  ];
+	public arrayCoracoes: Coracao[] = [
+		new Coracao(true),
+		new Coracao(true),
+		new Coracao(true)
+	];
 
-  constructor() {
-    console.log('tentativasComp > constructor: Corações[] ' + this.arrayCoracoes);
+	constructor()
+	{
+		console.log('tentativasComp > constructor: Corações[] ' + this.arrayCoracoes);
 
-  }
+	}
 
-  ngOnChanges() {
-    console.log('tentativasComp > OnChange: tentativas : ', this.tentativas);
+	ngOnChanges()
+	{
+		console.log('tentativasComp > OnChange: tentativas : ', this.tentativas);
 
-    if ( this.tentativas !== this.arrayCoracoes.length ) {
+		if (this.tentativas !== this.arrayCoracoes.length)
+		{
 
-      this.arrayCoracoes.forEach(coracao => {
-        let tirouCoracao = false;
-        if (coracao.cheio !== tirouCoracao) {
-          coracao.cheio = false;
-          tirouCoracao = false;
-        }
-      });
-      this.arrayCoracoes[0].cheio = false;
-    }
-  }
+			this.arrayCoracoes.forEach(coracao =>
+			{
+				let tirouCoracao = false;
+				if (coracao.cheio !== tirouCoracao)
+				{
+					coracao.cheio = false;
+					tirouCoracao = false;
+				}
+			});
+			this.arrayCoracoes[0].cheio = false;
+		}
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit()
+	{
+	}
 
 }

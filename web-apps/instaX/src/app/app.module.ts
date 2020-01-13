8d3da6e8f0a45817144ead65f322fa814faabf46
Alex Santos <arkanjo.alex@gmail.com>
2020-01-13 19:07:07 -0300

    Components Criados - Access - Banner - Login - SignUp

--- web-apps/instaX/src/app/app.module.ts
+++ web-apps/instaX/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccessComponent } from './access/access.component';
+import { BannerComponent } from './access/banner/banner.component';
+import { LoginComponent } from './access/login/login.component';
+import { SignUpComponent } from './access/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccessComponent,
+    BannerComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,

6e94fefdbf7f110c445a562028e44ed91b8f82d1
Alex Santos <arkanjo.alex@gmail.com>
2020-01-19 16:25:30 -0300

    ANIMATIONS - Primeira animacao OK

--- web-apps/instaX/src/app/access/banner/banner.component.ts
+++ web-apps/instaX/src/app/access/banner/banner.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+	selector: 'app-banner',
+	templateUrl: './banner.component.html',
+	styleUrls: ['./banner.component.scss'],
+	animations: [
+		trigger('bannerTrigger', [
+			state('escondido',
+				style({ opacity: 0 })),
+			state('visivel',
+				style({ opacity: 1 })),
+			transition('escondido <=> visivel', [
+				animate('1s ease-in')
+			])
+		])
+	]
+
 })
 export class BannerComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+	public stateStr: string = 'escondido';
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	private switchImage(): void {
+		this.stateStr = (this.stateStr === 'escondido') ? 'visivel' : 'escondido';
+		console.log(this.stateStr);
+	}
 
 }

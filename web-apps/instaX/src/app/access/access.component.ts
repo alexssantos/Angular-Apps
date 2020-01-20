59da4d41ddb3f154a84535c98db60871d37da958
Alex Santos <arkanjo.alex@gmail.com>
2020-01-19 23:29:03 -0300

    ANIMATON - fade-in Painel and Form

--- web-apps/instaX/src/app/access/access.component.ts
+++ web-apps/instaX/src/app/access/access.component.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
 	selector: 'app-access',
 	templateUrl: './access.component.html',
-	styleUrls: ['./access.component.scss']
+	styleUrls: ['./access.component.scss'],
+	animations: [
+		trigger('animation-banner', [
+			state('criado', style({
+				opacity: 1
+			})),
+			transition('void => criado', [
+				style({ opacity: 0, transform: 'translate(-50px,0)' }),
+				animate('500ms 0s ease-in-out') //duration, delay, acceleration
+			])
+		]),
+		trigger('animation-painel', [
+			state('criado', style({
+				opacity: 1
+			})),
+			transition('void => criado', [
+				style({ opacity: 0, transform: 'translate(50px,0)' }),
+				animate('500ms 0s ease-in-out') //duration, delay, acceleration
+			])
+		])
+	]
 })
 export class AccessComponent implements OnInit {
 
+	public stateBanner: string = 'criado';
+	public statePainel: string = 'criado';
 	public signUpPage: boolean = false;
 
 	constructor() { }

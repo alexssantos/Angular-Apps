cc27e4236e30d152a3628381a0acd4a2621ccf5a
Alex Santos <arkanjo.alex@gmail.com>
2020-02-29 23:52:55 -0300

    nx2-chart - bar chart - pie chart - radar chart - line chart

--- web-apps/charts-sample/src/app/charts/my-ng2-charts/my-ng2-charts.component.ts
+++ web-apps/charts-sample/src/app/charts/my-ng2-charts/my-ng2-charts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartType, ChartOptions } from 'chart.js';
-import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
+import { ChartType, ChartOptions, ChartDataSets, RadialChartOptions } from 'chart.js';
+import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip, Color } from 'ng2-charts';
 
 
 @Component({
@@ -10,7 +10,9 @@ import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsToolt
 })
 export class MyNg2ChartsComponent implements OnInit {
 
+	// ===================
 	// Pie
+	// ===================
 	public pieChartOptions: ChartOptions = {
 		responsive: true,
 	};
@@ -19,6 +21,58 @@ export class MyNg2ChartsComponent implements OnInit {
 	public pieChartType: ChartType = 'pie';
 	public pieChartLegend = true;
 	public pieChartPlugins = [];
+
+	// ===================
+	// bar
+	// ===================
+	public barChartOptions: ChartOptions = {
+		responsive: true,
+	};
+	public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+	public barChartType: ChartType = 'bar';
+	public barChartLegend = true;
+	public barChartPlugins = [];
+
+	public barChartData: ChartDataSets[] = [
+		{ data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+		{ data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+	];
+
+	// ===================
+	// line
+	// ===================
+	public lineChartData: ChartDataSets[] = [
+		{ data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+	];
+	public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+	public lineChartOptions: ChartOptions = {
+		responsive: true,
+	};
+	public lineChartColors: Color[] = [
+		{
+			borderColor: 'black',
+			backgroundColor: 'rgba(255,0,0,0.3)',
+		},
+	];
+	public lineChartLegend = true;
+	public lineChartType = 'line';
+	public lineChartPlugins = [];
+
+	// ===================
+	// radar
+	// ===================
+	public radarChartOptions: RadialChartOptions = {
+		responsive: true,
+	};
+	public radarChartLabels: Label[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
+
+	public radarChartData: ChartDataSets[] = [
+		{ data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A' },
+		{ data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B' }
+	];
+	public radarChartType: ChartType = 'radar';
+
 
 	constructor() {
 		monkeyPatchChartJsTooltip();

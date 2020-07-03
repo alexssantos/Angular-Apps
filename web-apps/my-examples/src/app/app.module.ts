9a00693cc59626747955fecf5d0568ea28ed1b32
Alex Santos <arkanjo.alex@gmail.com>
2020-07-03 14:58:13 -0300

    Header DONE

--- web-apps/my-examples/src/app/app.module.ts
+++ web-apps/my-examples/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +19,10 @@ import { HomeComponent } from './pages/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+	BrowserAnimationsModule,
+	MatButtonModule,
+	MatIconModule,
+	MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]

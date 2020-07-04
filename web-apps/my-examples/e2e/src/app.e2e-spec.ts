ce66a119bcbcc3c1d0a390d317b7445d179a8d2b
Alex Santos <arkanjo.alex@gmail.com>
2020-07-04 19:18:26 -0300

    Rename project

--- web-apps/my-examples/e2e/src/app.e2e-spec.ts
+++ web-apps/my-examples/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('my-examples app is running!');
+    expect(page.getTitleText()).toEqual('kakashi app is running!');
   });
 
   afterEach(async () => {

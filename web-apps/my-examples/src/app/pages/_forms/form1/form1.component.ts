468c5bffcef33f8ddaa1245832909c393d5970c3
Alex Santos <arkanjo.alex@gmail.com>
2020-07-07 16:19:32 -0300

    Pegando valores com JS e ngModel (ngForm)

--- web-apps/my-examples/src/app/pages/_forms/form1/form1.component.ts
+++ web-apps/my-examples/src/app/pages/_forms/form1/form1.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-form1',
-  templateUrl: './form1.component.html',
-  styleUrls: ['./form1.component.scss']
+	selector: 'app-form1',
+	templateUrl: './form1.component.html',
+	styleUrls: ['./form1.component.scss']
 })
 export class Form1Component implements OnInit {
 
-  constructor() { }
+	//@ViewChild('form2') meuFormulario: NgForm;
 
-  ngOnInit(): void {
-  }
+	constructor() { }
 
+	ngOnInit(): void {
+		this.oldSubmitHandler();
+	}
+
+	private oldSubmitHandler(): void {
+		window.addEventListener("load", function () {
+			document.getElementById('form1').addEventListener("submit", function (e) {
+				e.preventDefault(); // before the code
+				/* do what you want with the form */
+				
+				var form = document.forms['form1'];
+				console.log(form.elements);
+
+				for (var i = 0; i < form.elements.length; i++) {
+					var element = form.elements[i];
+					console.log(element.value);
+				}
+				
+				// Should be triggered on form submit
+				alert('hi');
+			});
+		});
+	}
+
+	public submitForm2(form: NgForm): void {
+		console.log(form.value);
+	}
 }

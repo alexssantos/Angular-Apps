cfc43846ce9a2e1bc2ff0a8198cb1c0af83bf98f
Alex Santos <arkanjo.alex@gmail.com>
2020-07-07 11:23:57 -0300

    ADD router - router no menu lateral

--- web-apps/my-examples/src/app/app.component.ts
+++ web-apps/my-examples/src/app/app.component.ts
@@ -9,7 +9,45 @@ import { MatDrawer } from '@angular/material/sidenav';
 export class AppComponent {
 
 	@ViewChild('drawer') drawer: MatDrawer;
-	today: number = Date.now();	
+	
+	public today: number = Date.now();	
+	public navLinks: any[];
 
-	constructor() {	}	
+	constructor() {	
+		this.generateNavlinks();
+	}	
+
+	public generateNavlinks(): void {
+		this.navLinks = [
+			{
+				label: 'Home',
+				link: '/home',
+				icon: 'home',
+				//index: 0
+			}, {
+				label: 'Forms',
+				link: '/forms',
+				icon: 'assignment',
+				//index: 1
+			}, {
+				label: 'Sidnavs',
+				link: '/sidenavs',
+				icon: 'menu',
+				//index: 2
+			},{
+				label: 'Animations',
+				link: '/',
+				icon: 'widgets',
+				//index: 2
+			},{
+				label: 'Configs',
+				link: '/',
+				icon: 'settings',
+				//index: 2
+			},
+		]
+
+		this.navLinks.forEach((val, i) => val.index = i);
+		console.log(this.navLinks);
+	}
 }
